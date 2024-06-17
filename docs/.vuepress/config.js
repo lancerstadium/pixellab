4fa761b059744fa3ebde961d56789e7b3e5e63a5
lancerstadium <lancerstadium@163.com>
2024-06-17 13:58:13 +0800

    Mon Jun 17 13:58:13 CST 2024

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,13 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
+
 export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: defaultTheme(),
+    bundler: viteBundler(),
+    theme: defaultTheme(),
+
+    lang: 'zh-CN',
+    title: 'Pixel Lab',
+    base: '/pixellab/' ,
+    description: 'Pixel Lab Website',
 })
